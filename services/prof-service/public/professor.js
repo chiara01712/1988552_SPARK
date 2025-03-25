68b74132877a7d6928c9504fcfe3b6267d36d9e4
AlessioCalcagni0 <calcagnialessio13@gmail.com>
2025-03-25 12:15:03 +0100

    Professor Page + Course Page Style

---
+++ services/prof-service/public/professor.js
@@ -0,0 +1,14 @@
+function openPopup(popup, overlay){
+    document.getElementById(popup).classList.add("popupactive");
+    document.getElementById(overlay).classList.add("overlayactive");
+}
+
+function closePopup(popup, overlay){
+    document.getElementById(popup).classList.remove("popupactive");
+    document.getElementById(overlay).classList.remove("overlayactive");
+}
+
+function addCourse(popup,overlay){
+    document.getElementById(popup).classList.remove("popupactive");
+    document.getElementById(overlay).classList.remove("overlayactive");
+}
