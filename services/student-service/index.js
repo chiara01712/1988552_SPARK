7a40526e572906248acc90074f64b2cfa95c00f7
ClaudiaCornacchia <cornacchia.1986434@studenti.uniroma1.it>
2025-03-29 18:24:47 +0100

    RabbitMQ student-service 2

--- services/student-service/index.js
+++ services/student-service/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const userRoutes = require('./src/student_route');
+const RabbitMQNote = require('./src/rabbitmq/note-s');
 
 const app = express();
 
@@ -17,10 +18,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Serve static files from the 'public' directory
 app.use('', userRoutes);
 
+// 1. When a post request is made to /operate, call the produce method of the RabbitMQNote class
+// go to the producer.js file and see the produce method
+
+app.post("/operate", async (req, res, next) => {
+  console.log("/operate",req.body);
+  console.log("Type of req.body:", typeof req.body);
+  try {
+    const response = await RabbitMQNote.produce(req.body);
+    res.send({ response });
+  } catch (error) {
+    next(error); 
+  }
+});
 
 const port = 7070;
-app.listen(port, () => {
+app.listen(port, async() => {
   console.log(`Student Server is running on port ${port}`);
+  await RabbitMQNote.initialize(); // Initialize the RabbitMQ connection only when the server starts
+  
 });
 
 
