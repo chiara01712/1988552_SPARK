2a82dffd40a3095e273ea61b1b5228588c1272bb
ClaudiaCornacchia <cornacchia.1986434@studenti.uniroma1.it>
2025-03-29 18:23:37 +0100

    RabbitMQ student-service side

---
+++ services/student-service/src/rabbitmq/producer.js
@@ -0,0 +1,49 @@
+const { randomUUID } = require('crypto');
+const EventEmitter = require('events');
+const config = require('../../config');
+
+class Producer {
+  constructor(channel, replyQueueName, eventEmitter) {
+    this.channel = channel;
+    this.replyQueueName = replyQueueName;
+    this.eventEmitter = eventEmitter;
+  }
+
+  async produceMessages(data) {
+    const uuid = randomUUID(); // Generate a random UUID
+    console.log("/producer1",data);
+    console.log("Type of data:", typeof data);
+
+    // Send the message to the RPC queue with the necessary properties
+    this.channel.sendToQueue(
+      config.rabbitMQ.queues.rpcQueue,  // 2. Queue to send the message to
+      Buffer.from(JSON.stringify(data)), // Data to be sent
+      {
+        // Queue in which the response will be sent by user-service
+        replyTo: this.replyQueueName, 
+        correlationId: uuid, // ID to correlate the request with the response
+        expiration: 30, // Time in milliseconds after which the message will be deleted
+        
+        // headers: {
+        //   function: data.operation,
+        // },
+      }
+    );
+
+    // Return a Promise that resolves when the response is received
+    return new Promise((resolve, reject) => {
+      // Listen for the response with the specified UUID
+      this.eventEmitter.once(uuid, async (data) => {
+        try {
+          // Analyze the response received from the consumer and resolve the Promise
+          const reply = JSON.parse(data.content.toString());
+          resolve(reply);
+        } catch (error) {
+          reject(error); // Reject the Promise if an error occurs
+        }
+      });
+    });
+  }
+}
+
+module.exports = Producer;
