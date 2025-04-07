de8e811e43011edd9e07039fc962040d69725ada
chiara01712 <167204113+chiara01712@users.noreply.github.com>
2025-04-07 19:01:53 +0200

    updt

--- services/student-service/public/student_home.js
+++ services/student-service/public/student_home.js
@@ -73,30 +73,32 @@ document.addEventListener('DOMContentLoaded', fetchNotes);
 function open_Menu() {
     document.getElementById("mySidebar").style.width = "25%";
     document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
+    //aprire il menu chiude il profilo
+    document.getElementById("profileSidebar").style.display = "none";
 }
 
 function close_Menu() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
 }
 function open_Profile() {
     document.getElementById("profileSidebar").style.width = "25%";
     document.getElementById("profileSidebar").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
+    //aprire il profilo chiude il menu
+    document.getElementById("mySidebar").style.display = "none";
 }
 function close_Profile() {
-    document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("profileSidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
 }
+
+
 
 document.addEventListener("DOMContentLoaded", function () {
     const modal = document.getElementById("noteModal");
     const openBtn = document.getElementById("openFormButton");
     const closeBtn = document.querySelector(".close-btn");
-
+    
+    
     // Open modal
     openBtn.addEventListener("click", function () {
         modal.style.display = "block";
@@ -112,6 +114,11 @@ document.addEventListener("DOMContentLoaded", function () {
         if (event.target === modal) {
             modal.style.display = "none";
         }
+        else if (!event.target === menu ){
+            document.getElementById("mySidebar").style.display = "none";
+        }
+        else if (!event.target === profile){
+            document.getElementById("profileSidebar").style.display = "none";        }
     });
 
     // Handle form submission
