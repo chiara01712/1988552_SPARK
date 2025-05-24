5198efc5c760ecdf8b6abd9e75843b60591b754b
chiara01712 <167204113+chiara01712@users.noreply.github.com>
2025-05-24 12:20:01 +0200

    Delete services directory

--- services/user-service/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    rabbitMQ: {
-      url: "amqp://rabbitmq",
-      queues: {
-        rpcQueue: "rpc_queue", // Queue for RPC communication (other services that need data from this service can use this queue)
-      },
-    },
-  };
-  
